2c9637f7a17d11af45b9112ccfeb7d0987b93147
kidrauhl123 <755439274@qq.com>
2026-04-03 23:03:06 +0800

    Update route.ts

--- smartcv-web/src/app/api/generate-latex/route.ts
+++ smartcv-web/src/app/api/generate-latex/route.ts
@@ -40,7 +40,7 @@ const SYSTEM_PROMPT = `你是一名专业的简历设计师，将用户的简历
 
 \\begin{minipage}[b]{0.72\\textwidth}
 {\\raggedright
-  {\\Huge \\bfseries \\color{black}{姓名} \\color{gray}{拼音名或英文名}} \\\\[0.8em]
+  {\\Huge \\bfseries \\color{black}{姓名} \\color{gray}{拼音或英文名}} \\\\[0.8em]
   {\\small \\color{black!80} \\makebox[1.5em][c]{\\faMobile} \\ 电话} \\\\[0.4em]
   {\\small \\color{black!80} \\makebox[1.5em][c]{\\faEnvelope} \\ \\href{mailto:邮箱}{邮箱}} \\\\[0.4em]
   % 其他联系方式（微信、GitHub 等）同格式追加，有什么放什么
